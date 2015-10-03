e80e4a05e7faa0120bc41dc7925e8f921155c05d
Peng.Li <peng.li@rea-group.com>
2015-10-03 13:14:03 +0800

    modify document

--- src/freewind/MyDocumentListener.java
+++ src/freewind/MyDocumentListener.java
@@ -1,13 +1,39 @@
 package freewind;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.event.DocumentAdapter;
 import com.intellij.openapi.editor.event.DocumentEvent;
 
 public class MyDocumentListener extends DocumentAdapter {
 
     @Override
-    public void documentChanged(DocumentEvent e) {
-        System.out.println("### " + Thread.currentThread().getName() + ", document changed: " + e);
+    public void documentChanged(final DocumentEvent e) {
+        System.out.println("### documentChanged, " + Thread.currentThread().getName() + ", document changed: " + e);
+        System.out.println(e.getDocument().getText());
+
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("### invokeLater, " + Thread.currentThread().getName());
+                        e.getDocument().setText(e.getDocument().getText() + "!!!!!!!!!!!!!!!!!!!!!");
+                    }
+                });
+
+            }
+        });
+
+        slowAction();
+    }
+
+    private void slowAction() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
